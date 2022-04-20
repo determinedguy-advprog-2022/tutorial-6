5da098df51867403155bf7f2ffd657b2381145ab
Muhammad Athallah <muhammad.athallah01@ui.ac.id>
2022-04-20 16:56:00 +0700

    Development

--- src/main/java/id/ac/ui/cs/tutorial6/controller/MusicPlayerController.java
+++ src/main/java/id/ac/ui/cs/tutorial6/controller/MusicPlayerController.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.tutorial6.controller;
 
-import id.ac.ui.cs.tutorial6.model.musicplayer.MusicPlayer;
 import id.ac.ui.cs.tutorial6.model.musicplayer.MusicPlayerState;
 import id.ac.ui.cs.tutorial6.model.playlist.Playlist;
 import id.ac.ui.cs.tutorial6.model.song.Song;
@@ -19,6 +18,12 @@ import java.util.Optional;
 @RequestMapping("/music-player")
 public class MusicPlayerController {
 
+    // Constants of string for params
+    private static final String SONGS_STRING = "songs";
+    private static final String PLAYLIST_STRING = "playlist";
+    private static final String REDIRECT_MUSIC_PLAYER_STRING = "redirect:/music-player";
+    private static final String REDIRECT_MUSIC_PLAYER_SLASH_STRING = "redirect:/music-player/";
+
     @Autowired
     private MusicService musicService;
 
@@ -31,16 +36,16 @@ public class MusicPlayerController {
     @GetMapping()
     public String homepage(Model model) {
         model.addAttribute("playlists", playlistService.getPlaylists());
-        model.addAttribute("songs", songService.getSongs());
+        model.addAttribute(SONGS_STRING, songService.getSongs());
         return "homepage_and_detail_playlist";
     }
 
     @GetMapping(value = "/{playlistId}")
     public String getSongsByPlaylist(@PathVariable String playlistId,
                                      Model model) {
-        Playlist playlist = playlistService.getPlaylistById(playlistId);
+        var playlist = playlistService.getPlaylistById(playlistId);
         model.addAttribute("currentPlaylist", playlist);
-        model.addAttribute("songs", playlist.getSongsSet());
+        model.addAttribute(SONGS_STRING, playlist.getSongsSet());
         return "homepage_and_detail_playlist";
     }
 
@@ -49,14 +54,14 @@ public class MusicPlayerController {
                               @PathVariable String songId,
                               @RequestParam String state,
                               Model model) {
-        Playlist playlist = playlistService.getPlaylistById(playlistId);
-        Song song = playlistService.getSongInPlaylist(playlist, songId);
+        var playlist = playlistService.getPlaylistById(playlistId);
+        var song = playlistService.getSongInPlaylist(playlist, songId);
 
         musicService.setting(playlist, song);
-        MusicPlayer musicPlayer = musicService.getMusicPlayer(state);
+        var musicPlayer = musicService.getMusicPlayer(state);
         MusicPlayerState currentState = musicPlayer.getCurrentState();
 
-        model.addAttribute("playlist", playlist);
+        model.addAttribute(PLAYLIST_STRING, playlist);
         model.addAttribute("song", song);
         model.addAttribute("state", currentState.toString());
         return "music_player";
@@ -65,33 +70,33 @@ public class MusicPlayerController {
     @GetMapping(value = "/add-playlist")
     public String createPlaylist(Model model) {
         model.addAttribute("newPlaylist", new Playlist());
-        return "form_playlist_or_song";
+        return "form_playlist";
     }
 
     @PostMapping(value = "/add-playlist")
     public String createPlaylist(@RequestParam String name) {
         playlistService.createPlaylist(name);
-        return "redirect:/music-player";
+        return REDIRECT_MUSIC_PLAYER_STRING;
     }
 
     @GetMapping(value = "/add-song")
     public String createSong(Model model) {
         model.addAttribute("genres", SongGenre.values());
         model.addAttribute("newSong", new Song());
-        return "form_playlist_or_song";
+        return "form_song";
     }
 
     @PostMapping(value = "/add-song")
     public String createSong(@RequestParam String name,
                              @RequestParam SongGenre genre) {
         songService.createSong(name, genre);
-        return "redirect:/music-player";
+        return REDIRECT_MUSIC_PLAYER_STRING;
     }
 
     @GetMapping(value = "/{playlistId}/add-song")
     public String addSongToPlaylist(@PathVariable String playlistId, Model model) {
-        model.addAttribute("playlist", playlistService.getPlaylistById(playlistId));
-        model.addAttribute("songs", songService.getSongs());
+        model.addAttribute(PLAYLIST_STRING, playlistService.getPlaylistById(playlistId));
+        model.addAttribute(SONGS_STRING, songService.getSongs());
         return "form_add_song_to_playlist";
     }
 
@@ -99,34 +104,34 @@ public class MusicPlayerController {
     public String addSongToPlaylist(@PathVariable String playlistId,
                                     @RequestParam(value="songId") String[] songId) {
         playlistService.addSongsToPlaylist(playlistId, songId);
-        return "redirect:/music-player/" + playlistId;
+        return REDIRECT_MUSIC_PLAYER_SLASH_STRING + playlistId;
     }
 
     @GetMapping(value = "/{playlistId}/delete-song")
     public String deleteSongFromPlaylist(@PathVariable String playlistId, Model model) {
-        Playlist playlist = playlistService.getPlaylistById(playlistId);
-        model.addAttribute("playlist", playlist);
-        model.addAttribute("songs", playlist.getSongsSet());
-        return "form_add_song_to_playlist";
+        var playlist = playlistService.getPlaylistById(playlistId);
+        model.addAttribute(PLAYLIST_STRING, playlist);
+        model.addAttribute(SONGS_STRING, playlist.getSongsSet());
+        return "form_delete_song_from_playlist";
     }
 
     @PostMapping(value = "/{playlistId}/delete-song")
     public String deleteSongFromPlaylist(@PathVariable String playlistId,
                                     @RequestParam(value="songId") String[] songId) {
         playlistService.deleteSongFromPlaylist(playlistId, songId);
-        return "redirect:/music-player/" + playlistId;
+        return REDIRECT_MUSIC_PLAYER_SLASH_STRING + playlistId;
     }
 
     @GetMapping(value = "/playlist/{playlistId}/delete-playlist")
     public String deletePlaylist(@PathVariable String playlistId) {
       playlistService.deletePlaylist(playlistId);
-      return "redirect:/music-player";
+      return REDIRECT_MUSIC_PLAYER_STRING;
     }
 
     @GetMapping(value = "/song/{songId}/delete-song")
     public String deleteSong(@PathVariable String songId) {
       songService.deleteSong(songId);
-      return "redirect:/music-player";
+      return REDIRECT_MUSIC_PLAYER_STRING;
     }
 
     @PutMapping(value = "/{playlistId}")
@@ -135,6 +140,6 @@ public class MusicPlayerController {
                                  @RequestParam(value="songId") String[] songId ) {
       playlistService.updatePlaylist(playlistId, Optional.ofNullable(name), Optional.empty());
       playlistService.addSongsToPlaylist(playlistId, songId);
-      return "redirect:/music-player/" + playlistId;
+      return REDIRECT_MUSIC_PLAYER_SLASH_STRING + playlistId;
     }
 }
